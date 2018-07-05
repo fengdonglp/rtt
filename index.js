2517a2d45b36e08ec2fd3907297049f4c73751c2
FD <18735610165@163.com>
2018-07-05 20:48:50 +0800

    模块划分，封装录音转文字类

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const Ff = require('./utils/ffmpeg');
+const BaiduSpeech  = require('./lib/BaiduSpeech');
+const baidu  = require('./config/baidu_sdk.json');
+const Ff = require('./lib/utils/ffmpeg');
 const ff = new Ff();
-const AipSpeechClient = require("baidu-aip-sdk").speech;
-const { APP_ID, API_KEY, SECRET_KEY }  = require('./config/baidu_sdk.json');
+const path = require('path');
 
-ff.input('./output/record/record0.m4a')
-  .metadata()
-  .then(res => {
-    console.dir(res);
-  });
+const baiduSpeech = new BaiduSpeech(baidu);
+const file_path = path.resolve(process.cwd(), './assets/recording.m4a');
 
-// 新建一个对象，建议只保存一个对象调用服务接口
-const client = new AipSpeechClient(APP_ID, API_KEY, SECRET_KEY);
-
-client.recognize(speech, format, 16000, {cuid: Math.random()});
+baiduSpeech.audio2Text(file_path).then(text => {
+  console.log(text);
+}).catch(console.log.bind(console));
